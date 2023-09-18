5548f82ab5e37248b0f24de0c6b1fc4e2af984dc
Jon Vivian <jonv@datacom.co.nz>
2023-09-18 22:53:22 +1200

    dvh test

--- components/slideshow/slide-container.tsx
+++ components/slideshow/slide-container.tsx
@@ -4,7 +4,7 @@ export const SlideContainer = ({ children, className }: CNProps) => {
   return (
     <main
       className={cn(
-        "max-h-screen w-full snap-y snap-mandatory snap-always overflow-y-scroll [-ms-overflow-style:'none'] [scrollbar-width:'none'] supports-[height:100svh]:max-h-[100svh] [&::-webkit-scrollbar]:hidden",
+        "h-screen w-full snap-y snap-mandatory snap-always overflow-y-scroll [-ms-overflow-style:'none'] [scrollbar-width:'none'] supports-[height:100svh]:h-[100svh] [&::-webkit-scrollbar]:hidden",
         className,
       )}
     >
