8e8dde6b294d1ccbf18a52633dca9ca126ce300a
Jon Vivian <jonv@datacom.co.nz>
2023-09-18 23:08:15 +1200

    dvh test

--- components/slideshow/slide-container.tsx
+++ components/slideshow/slide-container.tsx
@@ -2,7 +2,7 @@ import { CNProps } from "@/lib/utils"
 
 export const SlideContainer = ({ children }: CNProps) => {
   return (
-    <main className="h-screen w-full supports-[height:100dvh]:h-[100dvh]">
+    <main className="h-screen w-full supports-[height:100lvh]:h-[100lvh]">
       <div className="h-screen w-full snap-y snap-mandatory snap-always overflow-y-scroll [-ms-overflow-style:'none'] [scrollbar-width:'none'] supports-[height:100svh]:h-[100svh] [&::-webkit-scrollbar]:hidden">
         {children}
       </div>
