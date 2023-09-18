2a689afedbac714c8c0cb603cb63d4e21a8402e0
Jon Vivian <jonv@datacom.co.nz>
2023-09-18 21:49:45 +1200

    dvh test

--- components/slideshow/slide.tsx
+++ components/slideshow/slide.tsx
@@ -11,7 +11,7 @@ export const Slide = ({ children, className }: CNProps) => {
     <section
       ref={ref}
       className={cn(
-        "h-screen w-full snap-start border-4 border-green-700 transition-all duration-0 ease-in supports-[height:100dvh]:h-[100dvh] supports-[height:100dvh]:opacity-0",
+        "h-screen w-full snap-start border-4 border-green-700 transition-all duration-0 ease-in supports-[height:100dvh]:h-[100dvh] supports-[height:100dvh]:opacity-30",
         className,
         isVisible && "duration-300 supports-[height:100dvh]:opacity-100",
       )}
