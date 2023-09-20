9ff3d17fe57bf8b9c712b44f4090b9f5c1806a39
Jon Vivian <jonv@datacom.co.nz>
2023-09-20 21:29:29 +1200

    remove wait from generate episode server action

--- lib/forcem/actions.ts
+++ lib/forcem/actions.ts
@@ -4,9 +4,9 @@ import {
   GenerateEpisodeQuery,
   generateEpisode,
 } from "@/lib/forcem/generate-episode"
-import { wait } from "@/lib/utils"
 
 export const generateEpisodeAction = async (query: GenerateEpisodeQuery) => {
-  await wait(500)
+  // Uncomment so simulate a slow database
+  // await (await import("@/lib/utils")).wait(500)
   return generateEpisode(query)
 }
