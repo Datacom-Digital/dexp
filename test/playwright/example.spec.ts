c4e0fae2044bcba59ca373197a4122e32522eff0
Jon Vivian <2586037+4leite@users.noreply.github.com>
2023-09-23 14:55:33 +1200

    add playwright tests (#8)
    * add playwright tests
    
    * add playwright tests
    
    * add playwright tests
    
    * add playwright tests
    
    * add playwright tests
    
    * add playwright tests
    
    * add playwright tests
    
    * add forcem test
    
    * update playwright runner
    
    * update playwright runner
    
    * update playwright runner
    
    * update playwright runner

---
+++ test/playwright/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test"
+
+test("@smoke has title", async ({ page }) => {
+  await page.goto("/")
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Any Old Type/)
+})
+
+test("@smoke link", async ({ page }) => {
+  await page.goto("/")
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Slides" }).click()
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: /Episode/ })).toBeVisible()
+})
+
+test("@smoke forcem", async ({ page }) => {
+  await page.goto("/forcem")
+
+  // Click the get started link.
+  await page.getByRole("button", { name: "Generate" }).click()
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: /Episode/ })).toBeVisible()
+})
