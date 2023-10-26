e30185c920b76d692c46576706e93ec40e0228f4
Jon Vivian <jonv@datacom.co.nz>
2023-10-26 13:15:50 +1300

    TEST HEADER HANDLING NOT FOR PROD

--- app/(admin)/api/[...nextauth]/route.ts
+++ app/(admin)/api/[...nextauth]/route.ts
@@ -6,11 +6,20 @@ import { handlers } from "@/server/auth/auth"
 export const POST = handlers.POST
 
 export const GET = (req: NextRequest) => {
+  if (req.nextUrl.search && (req.method === "GET" || req.method === "HEAD")) {
+    console.log(req.method, {
+      userAgent: headers().get("user-agent"),
+      native: headers().get("x-native-host"),
+      search: req.nextUrl.searchParams,
+    })
+  }
   // Work around safe links scanning https://github.com/nextauthjs/next-auth/issues/1840#issuecomment-1746608163
   if (
     headers().get("user-agent")?.toLowerCase().includes("oneoutlook") ||
-    headers().get("x-native-host")?.toLowerCase().includes("oneoutlook")
+    headers().get("x-native-host")?.toLowerCase().includes("oneoutlook") ||
+    req.method === "HEAD"
   ) {
+    console.log("Safelink bot detected")
     return new Response("OK", {
       status: 200,
     })
