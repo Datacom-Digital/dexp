c99be001469324c201804fa24c44d9eec22c57ab
Jon Vivian <jonv@datacom.co.nz>
2023-11-10 00:17:51 +1300

    fix trailing newline in init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 SCOPE=datacom-digital
 EMAIL_FROM=no-reply@dexp.nz
 
@@ -19,44 +18,45 @@ curl -sSfL https://get.tur.so/install.sh | bash > /dev/null 2>&1 &
 CURL_PID=$!
 fi
 
-read -p "Project name: " PROJECT_NAME
-read -p "Domain: " DOMAIN
+PROJECT_NAME="${PWD##*/}"
+DOMAIN="$PROJECT_NAME.dexp.nz"
 read -p "Master email: " MASTER_EMAIL
+
+mv dexp.code-workspace $PROJECT_NAME.code-workspace
 
 wait $CURL_PID
 turso auth login
 
 wait $NPM_PID
-npx vercel login
-
 npx vercel project add $PROJECT_NAME --scope=$SCOPE
 npx vercel link -p $PROJECT_NAME --scope=$SCOPE --yes
 
 DEV_DB_NAME="$PROJECT_NAME-dev"
 turso db create $DEV_DB_NAME
-turso db show $DEV_DB_NAME --url | npx vercel env add TURSO_URL preview
-turso db tokens create $DEV_DB_NAME | npx vercel env add TURSO_TOKEN preview
+echo -n `turso db show $DEV_DB_NAME --url` | npx vercel env add TURSO_URL preview
+echo -n `turso db tokens create $DEV_DB_NAME` | npx vercel env add TURSO_TOKEN preview
 
 PROD_DB_NAME="$PROJECT_NAME-prod"
 turso db create $PROD_DB_NAME
-turso db show $PROD_DB_NAME --url | npx vercel env add TURSO_URL production
-turso db tokens create $PROD_DB_NAME | npx vercel env add TURSO_TOKEN production
+echo -n `turso db show $PROD_DB_NAME --url` | npx vercel env add TURSO_URL production
+echo -n `turso db tokens create $PROD_DB_NAME` | npx vercel env add TURSO_TOKEN production
 
-openssl rand -hex 32 | npx vercel env add AUTH_SECRET development
-openssl rand -hex 32 | npx vercel env add AUTH_SECRET preview
-openssl rand -hex 32 | npx vercel env add AUTH_SECRET production
+echo -n `openssl rand -hex 32` | npx vercel env add AUTH_SECRET development
+echo -n `openssl rand -hex 32` | npx vercel env add AUTH_SECRET preview
+echo -n `openssl rand -hex 32` | npx vercel env add AUTH_SECRET production
 
-echo "$DOMAIN" | npx vercel env add DOMAIN production
+echo -n "$DOMAIN" | npx vercel env add DOMAIN production
 
-echo "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL development
-echo "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL preview
-echo "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL production
+echo -n "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL development
+echo -n "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL preview
+echo -n "$MASTER_EMAIL" | npx vercel env add MASTER_EMAIL production
 
-echo "$EMAIL_FROM" | npx vercel env add EMAIL_FROM development
-echo "$EMAIL_FROM" | npx vercel env add EMAIL_FROM preview
-echo "$EMAIL_FROM" | npx vercel env add EMAIL_FROM production
+echo -n "$EMAIL_FROM" | npx vercel env add EMAIL_FROM development
+echo -n "$EMAIL_FROM" | npx vercel env add EMAIL_FROM preview
+echo -n "$EMAIL_FROM" | npx vercel env add EMAIL_FROM production
 
-npx vercel pull
+echo "Initialisation complete - add project id to github repository secrets:"
+echo "VERCEL_PROJECT_ID=$(sed -n "s/.*projectId.*\"\([^\"].*\)\".*/\1/p" .vercel/project.json)"
 
 echo "Initialisation complete - add project id to github repository secrets:"
 echo "VERCEL_PROJECT_ID=$(sed -n "s/.*projectId.*\"\([^\"].*\)\".*/\1/p" .vercel/project.json)"
